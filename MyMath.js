6dc4e261724b74c9fee5bcd3232970a4ce51cee5
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-26 22:51:49 +0200

    Add exports

---
+++ MyMath.js
@@ -0,0 +1,9 @@
+
+class MyMath{
+    static sum = (a,b) =>a+b
+    static sub = (c,d) =>c-d
+    static div = (i,f) =>i/f
+    static mult = (j,g) =>j*g
+};
+
+exports.MyMath = MyMath;
--- index.js
+++ index.js
@@ -8,3 +8,21 @@ REPL:
  loop - повтори
 
 */
+
+const {MyMath} = require('./MyMath')
+const {Component}  = require('./Component.js');
+
+const sum = MyMath.sum(2,2)
+console.log(sum);
+
+const mult = MyMath.mult(2,3)
+console.log(mult);
+
+const component = new Component()
+console.log(component);
+
+/*
+Створіть декілька функцій або класів, 
+спробуйте їх експортувати і імпортувати у index.js
+
+*/
