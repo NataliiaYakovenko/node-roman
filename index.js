2b460578442461f17cc2362f8bda0d210ef420c8
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-27 22:59:06 +0200

    Add mini практика

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ console.log(fs);
 
 const p = fs.readFile('./text.txt', 'utf-8');
 p.then(data=>console.log(data))
+
+const p1 = fs.readFile('./text2.txt', 'utf-8');
+p1.then(data=>console.log(data))
