64a6777c107edd2f786daabd638b18c3e432fd1b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-27 22:51:16 +0200

    Add readFile

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 'use strict'
 
-/*
-Коли ми працюємо із функцією
-how works REQUIRE
-require(path) має 5 етапів
-- resolving (шукає файл) -> loading (читає файл) ->
--> wrappening (обгортування у нодівський контекст) -> 
--> evaluation (код виконується) -> caching (кеширюється результат)
- 
-resolving:
-   1) Core modules - встроєні модулі
-   2) File:
-        .js || *.json
-   3) File:
-         package.json -> 'main'   
-         index.js || index.json
-   4) node_modules        
-   5) throw new Error
+const fs = require('fs').promises
+console.log(fs);
 
-*/
+const p = fs.readFile('./text.txt', 'utf-8');
+p.then(data=>console.log(data))
